cbbe9af2ed5308ef8793980c5093f8fc57e5a429
Dik Medvescek Murovec <dik.med.mur@gmail.com>
2019-05-16 22:36:40 +0200

    Added snackbar js

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # RAGE Room Landing Page
 > Edina soba, v kateri morate razbiti vse pred sabo.
+
+# Setup
+> npm install -g bower
+> bower install snackbar
---
+++ assets/js/mailchimp.js
@@ -0,0 +1,27 @@
+function showSnackbar() {
+  console.log("Test");
+  var pos = $(this).attr('data-pos');
+  var actionText = $(this).attr('data-actionText');
+  var actionColor = $(this).attr('data-actionColor');
+  if (actionText == null)
+    actionText = 'Dismiss'
+  if (actionColor == null)
+    actionColor = '#f66496';
+  var showAction = $(this).attr('data-showAction');
+  if (showAction == null)
+    showAction = true;
+  else if (showAction == 'true')
+    showAction = true;
+  else
+    showAction = false;
+
+  Snackbar.show({
+    text: 'Welcome! Thanks for checking out Snackbar',
+    showAction: showAction,
+    actionText: actionText,
+    actionTextColor: actionColor,
+    backgroundColor: '#232323',
+    width: 'auto',
+    pos: pos
+  });
+}
